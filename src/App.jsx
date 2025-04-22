b2c10e2a94a02997303225527822af1a33ec2ef8
Aagam Jain <83833927+aagamjainn@users.noreply.github.com>
2025-04-23 00:38:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MineTodo from './components/MineTodo'
+
+
+const App = () => {
+  return (
+    <div className='bg-stone-900 grid py-4 min-h-screen'>
+       <MineTodo/>
+    </div>
+  )
+}
+
+export default App
+
